b3e1c7b6cd73fb5758b1e2027afd91ba1036597b
maeloc <anxogomez@gmail.com>
2019-05-13 20:00:04 +0200

    engadir validacións para ssn e país na clase Passenger e test unitarios

--- src/main/java/com/pluralsight/test_pyramid_strategy/airport/Passenger.java
+++ src/main/java/com/pluralsight/test_pyramid_strategy/airport/Passenger.java
@@ -1,12 +1,28 @@
 package com.pluralsight.test_pyramid_strategy.airport;
 
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Passenger {
 
 	private String identifier;
 	private String name;
 	private String countryCode;
+	private String ssnRegex = "^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$";
+	private Pattern pattern = Pattern.compile(ssnRegex);
 
 	public Passenger(String identifier, String name, String countryCode) {
+		Matcher matcher = pattern.matcher(identifier);
+		if(!matcher.matches()) {
+			throw new RuntimeException("Invalid identifier");
+		}
+
+		if(!Arrays.asList(Locale.getISOCountries()).contains(countryCode)) {
+			throw new RuntimeException(("Invalid country code"));
+		}
+
 		this.identifier = identifier;
 		this.name = name;
 		this.countryCode = countryCode;
@@ -17,6 +33,10 @@ public class Passenger {
 	}
 
 	public void setIdentifier(String identifier) {
+		Matcher matcher = pattern.matcher(identifier);
+		if(!matcher.matches()) {
+			throw new RuntimeException("Invalid identifier");
+		}
 		this.identifier = identifier;
 	}
 
@@ -33,9 +53,11 @@ public class Passenger {
 	}
 
 	public void setCountryCode(String countryCode) {
+		if(!Arrays.asList(Locale.getISOCountries()).contains(countryCode)) {
+			throw new RuntimeException(("Invalid country code"));
+		}
 		this.countryCode = countryCode;
 	}
-
 
 	@Override
 	public String toString() {
--- src/test/java/com/pluralsight/test_pyramid_strategy/airport/PassengerTest.java
+++ src/test/java/com/pluralsight/test_pyramid_strategy/airport/PassengerTest.java
@@ -2,7 +2,7 @@ package com.pluralsight.test_pyramid_strategy.airport;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PassengerTest {
 
@@ -12,4 +12,70 @@ public class PassengerTest {
                 "John Smith", "US");
         assertNotNull(passenger);
     }
+
+    @Test
+    public void testInvalidSsn() {
+        assertThrows(RuntimeException.class,
+                () -> {
+                    Passenger passenger = new Passenger("123-456-789",
+                            "John Smith", "US");
+                });
+    }
+
+    @Test
+    public void testSetInvalidSsn() {
+        assertThrows(RuntimeException.class,
+                () -> {
+                    Passenger passenger = new Passenger("123-45-6789",
+                            "John Smith", "US");
+                    passenger.setIdentifier("123-456-789");
+                });
+    }
+
+    @Test
+    public void testSetValidSsn() {
+        Passenger passenger = new Passenger("123-45-6789",
+                "John Smith", "US");
+        passenger.setIdentifier("123-98-7654");
+        assertEquals("123-98-7654", passenger.getIdentifier());
+    }
+
+    @Test
+    public void testInvalidCountryCode() {
+        assertThrows(RuntimeException.class,
+                () -> {
+                    Passenger passenger = new Passenger("123-45-6789",
+                            "John Smith", "GJ");
+                });
+    }
+
+    @Test
+    public void testSetInvalidCountryCode() {
+        assertThrows(RuntimeException.class,
+                () -> {
+                    Passenger passenger = new Passenger("123-45-6789",
+                            "John Smith", "US");
+                    passenger.setCountryCode("GJ");
+                });
+    }
+
+    @Test
+    public void testSetValidCountryCode() {
+        Passenger passenger = new Passenger("123-45-6789",
+                "John Smith", "US");
+        passenger.setCountryCode("ES");
+        assertEquals("ES", passenger.getCountryCode());
+    }
+
+    @Test
+    public void testPassengerToString() {
+        Passenger passenger = new Passenger("123-45-6789",
+                "John Smith", "US");
+        passenger.setName("John Brown");
+        passenger.recordToSystem();
+        assertEquals("Passenger John Brown with identifier: 123-45-6789 from US",
+                passenger.toString());
+    }
+
 }
+
